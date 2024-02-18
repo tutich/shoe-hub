b79fd174d6b03dac83e1767833c1cbf74bb4c16b
tutich <pkoinange02@gmail.com>
2024-02-18 23:51:47 +0300

    react components, data

---
+++ client/src/Data/Data.jsx
@@ -0,0 +1,28 @@
+
+
+const data = [
+    {
+        img: "client/src/dataimg/P-IMG-20230717-WA0012.jpg",
+        title: "Vans Off the Wall ",
+        star: <AiFillStar className="rating-star" />,
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "VANS",
+        color: "Green",
+        category: "Deck",
+      },
+
+      {
+        img: "client/src/dataimg/P-IMG-20230912-WA0047.jpg",
+        title: "Vans Off the Wall",
+        star: <AiFillStar className="rating-star" />,
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "VANS",
+        color: "Marron",
+        category: "sneakers",
+      },
+      
+]
