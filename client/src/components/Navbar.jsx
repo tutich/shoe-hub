1b5a93c1d0a97edac9f2aa7a8d79acff64b37486
tutich <pkoinange02@gmail.com>
2024-02-19 23:52:23 +0300

    navbar css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import {  FaCartPlus } from 'react-icons/fa6';
-
+import { CiHeart} from 'react-icons/ci'
+import { MdPerson} from 'react-icons/md'
 
  function Navbar() {
     return(
         <div>
-            <input
-            type='text'
-            placeholder='Search item'
-             />
-            <FaCartPlus />
+            <nav>
+                <div>
+                    <input
+                    className="search"
+                    type='text'
+                    placeholder='Search item'
+                     />
+                </div>
+                <div className="nav-icons">
+                    <div className="">
+                        <a href="" className='icon'>
+                            <FaCartPlus size={28} />
+                        </a>
+                    </div>
+                    <div className="icon">
+                        <a href="" className='icon'>
+                            <CiHeart size={28} />
+                        </a>
+                    </div>
+                    <div className="icon">
+                        <a href="" className='icon'>
+                            <MdPerson size={28}/>
+                        </a>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
  }
