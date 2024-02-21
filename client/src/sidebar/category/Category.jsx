9ae9d00ac73186ad4697dc522836a6441ab41147
tutich <pkoinange02@gmail.com>
2024-02-21 14:02:12 +0300

    category jsx, css

--- client/src/sidebar/category/Category.jsx
+++ client/src/sidebar/category/Category.jsx
@@ -1,11 +1,51 @@
+import './Category.css'
+
 
 function Category() {
     return (
         <div>
             <h2 className="title">Category</h2>
-            <div>
-                <label>
+            <div >
+                <label className='adidas'>
                     <span></span>
+                    All
+                    <input className='small' type="radio" name="text" />
+                </label>
+
+                <label className="adidas">
+                    <span></span>
+                    Adidas
+                    <input className='small' type="radio" name="text" />
+                </label>
+
+                <label className='adidas'>
+                    <span></span>
+                    Jordans
+                    <input className='small' type="radio" name="text" />
+                </label>
+
+                <label className="adidas">
+                    <span></span>
+                    Louis Vuiton
+                    <input className='small' type="radio" name="text" />
+                </label>
+
+                <label className="adidas">
+                    <span></span>
+                    Nike
+                    <input className='small' type="radio" name="text" />
+                </label>
+
+                <label className="adidas">
+                    <span></span>North Star
+                    
+                    <input className='small' type="radio" name="text" />
+                </label>
+                
+                <label className="adidas">
+                    <span></span>
+                    Puma
+                    <input className='small' type="radio" name="text" />
                 </label>
             </div>
         </div>
